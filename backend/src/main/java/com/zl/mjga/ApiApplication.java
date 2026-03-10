e0123b2ff4aa5eede18ff5b61e205f44c455d4e9
roc <lipeng1080@gmail.com>
2026-03-10 17:07:35 +0800

    fix(dubbo): 启动类启用Dubbo

--- backend/src/main/java/com/zl/mjga/ApiApplication.java
+++ backend/src/main/java/com/zl/mjga/ApiApplication.java
@@ -1,9 +1,11 @@
 package com.zl.mjga;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.zl.mjga", "org.jooq.generated"})
+@EnableDubbo(scanBasePackages = {"com.zl.mjga.service"})
 public class ApiApplication {
 
     public static void main(String[] args) {
