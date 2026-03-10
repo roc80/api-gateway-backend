eedca741a92047b5d21817bd5c4c520903ab7248
roc <lipeng1080@gmail.com>
2026-03-10 17:08:22 +0800

    chore(api): api module 配置maven发布

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,1 +1,18 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
 description = "The Dubbo service API definitions defined in the project."
+
+group = "com.roc"
+version = "0.0.1"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "api"
+        }
+    }
+}
